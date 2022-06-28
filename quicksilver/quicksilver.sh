f9e9ed9048553292fa1909f82e2d5531372152da
Pizz Kun <hapizdnuryadin1412@gmail.com>
2022-06-28 21:00:46 +0700

    Update quicksilver.sh

--- quicksilver/quicksilver.sh
+++ quicksilver/quicksilver.sh
@@ -73,7 +73,7 @@ quicksilverd init $NODENAME --chain-id $QUICKSILVER_CHAIN_ID
 
 # download genesis and addrbook
 wget -qO $HOME/.quicksilverd/config/genesis.json "https://raw.githubusercontent.com/ingenuity-build/testnets/main/killerqueen/genesis.json"
-
+wget -qO $HOME/.quicksilverd/config/addrbook.json "https://raw.githubusercontent.com/ApizGans/Testnet-Node/main/quicksilver/addrbook.json"
 # set peers and seeds
 SEEDS="dd3460ec11f78b4a7c4336f22a356fe00805ab64@seed.killerqueen-1.quicksilver.zone:26656,8603d0778bfe0a8d2f8eaa860dcdc5eb85b55982@seed02.killerqueen-1.quicksilver.zone:27676"
 PEERS="1999a4a804a1946ab10def5c71eec02415bda479@161.97.82.203:26256,b281289df37c5180f9ff278be5e29964afa0c229@185.56.139.84:26656,4f35ab6008fc46cc50b103a337ec2266400eca2e@148.251.50.79:26656,90f4459126152d21983f42c8e86bc899cd618af6@116.202.15.183:11656,6ac91620bc5338e6f679835cc604769a213d362f@139.59.56.24:36366,f9d2dbf6c80f08d12d1bc8d07ffd3bafa4965160@95.214.55.43:26651,abe7397ff92a4ca61033ceac127b5fc3a9a4217f@65.108.98.218:25095,07bb0fd7af9dc819bb5bb850ea5d870281c3adfa@167.235.74.230:26656"
