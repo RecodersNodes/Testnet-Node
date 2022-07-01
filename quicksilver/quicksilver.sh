6bfd766dbae6997a50cb43c2458d8afd3d4ff13c
Pizz Kun <hapizdnuryadin1412@gmail.com>
2022-07-01 15:28:38 +0700

    Update quicksilver.sh

--- quicksilver/quicksilver.sh
+++ quicksilver/quicksilver.sh
@@ -54,7 +54,7 @@ rm quicksilver -rf
 git clone https://github.com/ingenuity-build/quicksilver.git --branch v0.4.0
 cd quicksilver
 make install
-sudomv ~go/bin/quicksilverd /usr/local/bin/quicksilverd
+sudo mv ~go/bin/quicksilverd /usr/local/bin/quicksilverd
 
 # config
 quicksilverd config chain-id $QUICKSILVER_CHAIN_ID
