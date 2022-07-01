ad1408df535834183a27e782df401b0ff38f79b5
Pizz Kun <hapizdnuryadin1412@gmail.com>
2022-07-01 15:03:45 +0700

    Update quicksilver.sh

--- quicksilver/quicksilver.sh
+++ quicksilver/quicksilver.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 echo "================================================================="
 echo -e "\033[0;35m"
-echo "  _        _______  _______  _______    ______   _______  _______  ";
-echo " ( (    /|(  ___  )(  ____ \(  ___  )  (  __  \ (  ___  )(  ___  ) ";
-echo " |  \  ( || (   ) || (    \/| (   ) |  | (  \  )| (   ) || (   ) | ";
-echo " |   \ | || (___) || |      | (___) |  | |   ) || (___) || |   | | ";
-echo " | (\ \) ||  ___  || | ____ |  ___  |  | |   | ||  ___  || |   | | ";
-echo " | | \   || (   ) || | \_  )| (   ) |  | |   ) || (   ) || |   | | ";
-echo " | )  \  || )   ( || (___) || )   ( |  | (__/  )| )   ( || (___) | ";
-echo " |/    )_)|/     \|(_______)|/     \|  (______/ |/     \|(_______) ";
+echo "███████╗████████╗██████╗ ███████╗███████╗███████╗     ██████╗ █████╗ ██████╗ ██╗████████╗ █████╗ ██╗  ";
+echo "██╔════╝╚══██╔══╝██╔══██╗██╔════╝██╔════╝██╔════╝    ██╔════╝██╔══██╗██╔══██╗██║╚══██╔══╝██╔══██╗██║  ";
+echo "███████╗   ██║   ██████╔╝█████╗  ███████╗███████╗    ██║     ███████║██████╔╝██║   ██║   ███████║██║  ";
+echo "╚════██║   ██║   ██╔══██╗██╔══╝  ╚════██║╚════██║    ██║     ██╔══██║██╔═══╝ ██║   ██║   ██╔══██║██║  ";
+echo "███████║   ██║   ██║  ██║███████╗███████║███████║    ╚██████╗██║  ██║██║     ██║   ██║   ██║  ██║███████╗  ";
+echo "╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝     ╚═════╝╚═╝  ╚═╝╚═╝     ╚═╝   ╚═╝   ╚═╝  ╚═╝╚══════╝  ";
 echo -e "\e[0m"
 echo "================================================================="
 
@@ -16,121 +14,99 @@ sleep 2
 
 # set vars
 if [ ! $NODENAME ]; then
-	read -p "Masukan Nama Node: " NODENAME
+	read -p "Masukan Nama Node/Validators: " NODENAME
 	echo 'export NODENAME='$NODENAME >> $HOME/.bash_profile
 fi
-QUICKSILVER_PORT=11
-if [ ! $WALLET ]; then
-	echo "export WALLET=wallet" >> $HOME/.bash_profile
-fi
+
 echo "export QUICKSILVER_CHAIN_ID=killerqueen-1" >> $HOME/.bash_profile
-echo "export QUICKSILVER_PORT=${QUICKSILVER_PORT}" >> $HOME/.bash_profile
 source $HOME/.bash_profile
 
 echo '================================================='
 echo -e "Nama Node: \e[1m\e[32m$NODENAME\e[0m"
-echo -e "Nama Wallet: \e[1m\e[32m$WALLET\e[0m"
 echo -e "Nama Chain: \e[1m\e[32m$QUICKSILVER_CHAIN_ID\e[0m"
-echo -e "Port: \e[1m\e[32m$QUICKSILVER_PORT\e[0m"
 echo '================================================='
 sleep 2
 
-echo -e "\e[1m\e[32m1. Mengupdate Package... \e[0m" && sleep 1
+echo -e "\e[1m\e[32m1. Lagi Update... \e[0m" && sleep 1
 # update
 sudo apt update && sudo apt upgrade -y
+sudo apt autoremove -y
 
 echo -e "\e[1m\e[32m2. Menginstall Pendukung... \e[0m" && sleep 1
 # packages
-sudo apt install curl build-essential git wget jq make gcc tmux -y
+sudo apt install make clang pkg-config libssl-dev build-essential git jq llvm libudev-dev -y
 
+echo -e "\e[1m\e[32m2. Menginstall Golang... \e[0m" && sleep 1
 # install go
-ver="1.18.2"
-cd $HOME
-wget "https://golang.org/dl/go$ver.linux-amd64.tar.gz"
-sudo rm -rf /usr/local/go
-sudo tar -C /usr/local -xzf "go$ver.linux-amd64.tar.gz"
-rm "go$ver.linux-amd64.tar.gz"
-echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
-source ~/.bash_profile
+wget https://go.dev/dl/go1.18.1.linux-amd64.tar.gz
+sudo tar -xvf go1.18.1.linux-amd64.tar.gz && sudo mv go /usr/local
+&& echo "export PATH=$PATH:/usr/local/go/bin:$HOME/go/bin" >> ~/.bash_profile
+source ~/.bash_profile 
 go version
 
-echo -e "\e[1m\e[32m3. Sedang Mendownload Project Sabar Ya gan... \e[0m" && sleep 1
+rm go1.18.1.linux-amd64.tar.gz
+
+echo -e "\e[1m\e[32m3. Mengclone... \e[0m" && sleep 1
 # download binary
 cd $HOME
 rm quicksilver -rf
 git clone https://github.com/ingenuity-build/quicksilver.git --branch v0.4.0
 cd quicksilver
-make build
-sudo chmod +x ./build/quicksilverd && sudo mv ./build/quicksilverd /usr/local/bin/quicksilverd
+make install
+sudomv ~go/bin/quicksilverd /usr/local/bin/quicksilverd
 
 # config
 quicksilverd config chain-id $QUICKSILVER_CHAIN_ID
-quicksilverd config keyring-backend test
-quicksilverd config node tcp://localhost:${QUICKSILVER_PORT}657
 
 # init
 quicksilverd init $NODENAME --chain-id $QUICKSILVER_CHAIN_ID
 
 # download genesis and addrbook
 wget -qO $HOME/.quicksilverd/config/genesis.json "https://raw.githubusercontent.com/ingenuity-build/testnets/main/killerqueen/genesis.json"
-wget -qO $HOME/.quicksilverd/config/addrbook.json "https://raw.githubusercontent.com/ApizGans/Testnet-Node/main/quicksilver/addrbook.json"
+
+quicksilverd tendermint unsafe-reset-all --home $HOME/.quicksilverd
+
+echo -e "\e[1m\e[32m4. Memasang Service... \e[0m" && sleep 1
+# create service
+sudo tee /etc/systemd/system/quicksilverd.service > /dev/null <<EOF
+[Unit]
+Description=quicksilverd
+After=network.target
+[Service]
+Type=simple
+User=$USER
+ExecStart=$(which quicksilverd) start
+Restart=on-failure
+RestartSec=10
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+SNAP_RPC1="https://quicksilver-rpc.theamsolutions.info:443"
+SNAP_RPC2="https://quicksilver-rpc.theamsolutions.info:443"
+
+LATEST_HEIGHT=$(curl -s $SNAP_RPC2/block | jq -r .result.block.header.height); \
+BLOCK_HEIGHT=$((LATEST_HEIGHT - 1000)); \
+TRUST_HASH=$(curl -s "$SNAP_RPC2/block?height=$BLOCK_HEIGHT" | jq -r .result.block_id.hash)
+
+sed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1true| ; \
+s|^(rpc_servers[[:space:]]+=[[:space:]]+).*$|\1\"$SNAP_RPC1,$SNAP_RPC2\"| ; \
+s|^(trust_height[[:space:]]+=[[:space:]]+).*$|\1$BLOCK_HEIGHT| ; \
+s|^(trust_hash[[:space:]]+=[[:space:]]+).*$|\1\"$TRUST_HASH\"|" $HOME/.quicksilverd/config/config.toml
+
 # set peers and seeds
 SEEDS="dd3460ec11f78b4a7c4336f22a356fe00805ab64@seed.killerqueen-1.quicksilver.zone:26656,8603d0778bfe0a8d2f8eaa860dcdc5eb85b55982@seed02.killerqueen-1.quicksilver.zone:27676"
 PEERS="1999a4a804a1946ab10def5c71eec02415bda479@161.97.82.203:26256,b281289df37c5180f9ff278be5e29964afa0c229@185.56.139.84:26656,4f35ab6008fc46cc50b103a337ec2266400eca2e@148.251.50.79:26656,90f4459126152d21983f42c8e86bc899cd618af6@116.202.15.183:11656,6ac91620bc5338e6f679835cc604769a213d362f@139.59.56.24:36366,f9d2dbf6c80f08d12d1bc8d07ffd3bafa4965160@95.214.55.43:26651,abe7397ff92a4ca61033ceac127b5fc3a9a4217f@65.108.98.218:25095,07bb0fd7af9dc819bb5bb850ea5d870281c3adfa@167.235.74.230:26656"
 sed -i -e "s/^seeds *=.*/seeds = \"$SEEDS\"/; s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.quicksilverd/config/config.toml
 
-# set custom ports
-sed -i.bak -e "s%^proxy_app = \"tcp://127.0.0.1:26658\"%proxy_app = \"tcp://127.0.0.1:${QUICKSILVER_PORT}658\"%; s%^laddr = \"tcp://127.0.0.1:26657\"%laddr = \"tcp://127.0.0.1:${QUICKSILVER_PORT}657\"%; s%^pprof_laddr = \"localhost:6060\"%pprof_laddr = \"localhost:${QUICKSILVER_PORT}060\"%; s%^laddr = \"tcp://0.0.0.0:26656\"%laddr = \"tcp://0.0.0.0:${QUICKSILVER_PORT}656\"%; s%^prometheus_listen_addr = \":26660\"%prometheus_listen_addr = \":${QUICKSILVER_PORT}660\"%" $HOME/.quicksilverd/config/config.toml
-sed -i.bak -e "s%^address = \"tcp://0.0.0.0:1317\"%address = \"tcp://0.0.0.0:${QUICKSILVER_PORT}317\"%; s%^address = \":8080\"%address = \":${QUICKSILVER_PORT}080\"%; s%^address = \"0.0.0.0:9090\"%address = \"0.0.0.0:${QUICKSILVER_PORT}090\"%; s%^address = \"0.0.0.0:9091\"%address = \"0.0.0.0:${QUICKSILVER_PORT}091\"%" $HOME/.quicksilverd/config/app.toml
-
-# disable indexing
-indexer="null"
-sed -i -e "s/^indexer *=.*/indexer = \"$indexer\"/" $HOME/.quicksilverd/config/config.toml
-
-# config pruning
-pruning="custom"
-pruning_keep_recent="100"
-pruning_keep_every="0"
-pruning_interval="50"
-sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.quicksilverd/config/app.toml
-sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.quicksilverd/config/app.toml
-sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.quicksilverd/config/app.toml
-sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.quicksilverd/config/app.toml
-
-# set minimum gas price
-sed -i -e "s/^minimum-gas-prices *=.*/minimum-gas-prices = \"0uqck\"/" $HOME/.quicksilverd/config/app.toml
-
-# enable prometheus
-sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.quicksilverd/config/config.toml
-
-# reset
-quicksilverd tendermint unsafe-reset-all --home $HOME/.quicksilverd
-
-echo -e "\e[1m\e[32m4. Menjalankan Service... \e[0m" && sleep 1
-# create service
-sudo tee /etc/systemd/system/quicksilverd.service > /dev/null <<EOF
-[Unit]
-Description=quicksilver
-After=network-online.target
-
-[Service]
-User=$USER
-ExecStart=$(which quicksilverd) --home $HOME/.quicksilverd start
-Restart=on-failure
-RestartSec=3
-LimitNOFILE=65535
-
-[Install]
-WantedBy=multi-user.target
-EOF
 
 # start service
 sudo systemctl daemon-reload
 sudo systemctl enable quicksilverd
 sudo systemctl restart quicksilverd
 
-echo '=============== Sudah Terinstall ==================='
+echo '=============== Selamat Anda Jadi Heker  ==================='
 echo -e 'untuk check logs: \e[1m\e[32mjournalctl -u quicksilverd -f -o cat\e[0m'
-echo -e "untuk check sync status: \e[1m\e[32mcurl -s localhost:${QUICKSILVER_PORT}657/status | jq .result.sync_info atau quicksilverd status 2>&1 | jq .SyncInfo\e[0m"
-echo -e 'untuk menambahkan wallet: \e[1m\e[32mquicksilverd keys add namawallet\e[0m'
-echo -e 'untuk menambahkan wallet yang sudah dibuat: \e[1m\e[32mquicksilverd keys add namawallet --recover\e[0m'
+echo -e 'Info: \e[1m\e[32mNunggu Sekitar 10-15 menit mungkin\e[0m'
+echo -e 'kalau udah ke commit sync masukan command ini: \e[1m\e[32msed -i.bak -E "s|^(enable[[:space:]]+=[[:space:]]+).*$|\1false|" $HOME/.quicksilverd/config/config.toml\e[0m'
